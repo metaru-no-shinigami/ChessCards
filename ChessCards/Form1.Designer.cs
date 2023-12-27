3ec88e7f4b21050b68306fb86d78a3a64d3ffb4d
Sir.Gregory.B.Ryan@gmail.com <Sir.Gregory.B.Ryan@gmail.com>
2023-12-27 00:26:04 -0500

    Changes

--- ChessCards/Form1.Designer.cs
+++ ChessCards/Form1.Designer.cs
@@ -29,50 +29,36 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChessCards));
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btn_Submit = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btn_FEN = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // btn_Submit
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.btn_Submit.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button1.Location = new System.Drawing.Point(12, 730);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(600, 50);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Submit";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(12, 786);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(600, 30);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Done";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.btn_Submit.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Submit.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_Submit.Location = new System.Drawing.Point(12, 756);
+            this.btn_Submit.Name = "btn_Submit";
+            this.btn_Submit.Size = new System.Drawing.Size(600, 50);
+            this.btn_Submit.TabIndex = 0;
+            this.btn_Submit.Text = "Stats";
+            this.btn_Submit.UseVisualStyleBackColor = true;
+            this.btn_Submit.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(12, 702);
+            this.textBox1.Location = new System.Drawing.Point(12, 728);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(600, 22);
             this.textBox1.TabIndex = 2;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // pictureBox1
             // 
@@ -83,38 +69,51 @@
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
-            // button3
+            // btn_FEN
             // 
-            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.btn_FEN.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.button3.Cursor = System.Windows.Forms.Cursors.Default;
-            this.button3.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.ForeColor = System.Drawing.Color.Maroon;
-            this.button3.Location = new System.Drawing.Point(12, 12);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(600, 30);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Generate FEN";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.btn_FEN.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btn_FEN.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_FEN.ForeColor = System.Drawing.Color.Maroon;
+            this.btn_FEN.Location = new System.Drawing.Point(12, 12);
+            this.btn_FEN.Name = "btn_FEN";
+            this.btn_FEN.Size = new System.Drawing.Size(600, 30);
+            this.btn_FEN.TabIndex = 4;
+            this.btn_FEN.Text = "Generate FENs";
+            this.btn_FEN.UseVisualStyleBackColor = true;
+            this.btn_FEN.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 701);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Score:";
             // 
             // ChessCards
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(622, 828);
-            this.Controls.Add(this.button3);
+            this.ClientSize = new System.Drawing.Size(622, 820);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_FEN);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_Submit);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "ChessCards";
             this.Text = "Chess Cards";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ChessCards_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -122,12 +121,11 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btn_Submit;
+        private System.Windows.Forms.Button btn_FEN;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- ChessCards/Form1.cs
+++ ChessCards/Form1.cs
@@ -7,30 +7,126 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace ChessCards
 {
     public partial class ChessCards : Form
     {
+
+        public class Position
+        {
+            public int Catagory { get; set; }
+            public int Successes { get; set; }
+            public int TimesPlayed { get; set; }
+            public string PositionURL { get; set; }
+            public string Move { get; set; }
+        }
+
+        public class Flashcard
+        {
+            public List<Position> Position { get; set; }
+        }
+
+        public Position NewCard { get; set; }
+        List<int> CompletedCards = new List<int>();
+        private int Correct = 0;
+        private int Total = 0;
+        private int CardNum = 0;
+        private readonly string path = @"C:\Users\SirGr\source\repos\ChessCards\ChessCards\ChessCardsData.txt";
+
         public ChessCards()
         {
             InitializeComponent();
-            pictureBox1.Load("https://fen2image.chessvision.ai/rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR_w_KQkq_-_0_1?turn=white");
+            this.KeyPreview = true;
+            GenerateNewCard();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        public void GenerateNewCard()
         {
+            string json = File.ReadAllText(path);
+            var CardData = JsonConvert.DeserializeObject<Flashcard>(json);
+            List<Position> SetData = CardData.Position;
+            Random random = new Random();
+            int TempCatagory = random.Next(1, 6);
+            List<Position> CatagorySet = new List<Position>();
+            foreach (Position Card in SetData)
+            {
+                if (Card.Catagory <= TempCatagory)
+                {
+                    CatagorySet.Add(Card);
+                }
+            }    
+            int size = CatagorySet.Count;
+            if (size == 0)
+            {
+                CatagorySet = SetData;
+                size = CatagorySet.Count;
+            }
+            int CardTempNum = random.Next(size);
+            NewCard = CatagorySet[CardTempNum];
+            CardNum = SetData.IndexOf(NewCard);
+            while (CompletedCards.Count < SetData.Count && CompletedCards.Contains(CardNum))
+            {
+                CardTempNum = random.Next(size);
+                NewCard = CatagorySet[CardTempNum];
+                CardNum = SetData.IndexOf(NewCard);
+            }  
+            if (CompletedCards.Count == SetData.Count)
+            {
+                int Percent = (int)Math.Round((decimal) 100 * Correct / Total);
+                MessageBox.Show($"All cards complete! Your score is {Percent}% ({Correct}/{Total})", "Message");
+            }
+            else
+            {
+                pictureBox1.Load(NewCard.PositionURL);
+            }
+            if (CompletedCards.Contains(CardNum) == false)
+            {
+                CompletedCards.Add(CardNum);
+            }
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void ChessCards_KeyDown(object sender, KeyEventArgs e)
         {
-            
+            string json = File.ReadAllText(path);
+            var TempData = JsonConvert.DeserializeObject<Flashcard>(json);
+            if (e.KeyCode == Keys.Enter && CompletedCards.Count <= TempData.Position.Count && textBox1.Text != null)
+            {
+                Total++;
+                NewCard.TimesPlayed++;
+                if (textBox1.Text == NewCard.Move)
+                {
+                    Correct++;
+                    NewCard.Successes++;
+                }
+                int SuccessRate = (int)Math.Round((decimal)100 * NewCard.Successes / NewCard.TimesPlayed);
+                NewCard.Catagory = 5 - (int)Math.Floor((decimal)(100 - SuccessRate) / 20);
+                TempData.Position[CardNum] = NewCard;
+                string WriteTempData = JsonConvert.SerializeObject(TempData, Formatting.Indented);
+                File.WriteAllText(path, WriteTempData);
+                textBox1.Clear();
+                int Percent = (int)Math.Round((decimal)100 * Correct / Total);
+                label1.Text = $"Score: {Percent}% ({Correct}/{Total})";
+                GenerateNewCard();
+                e.SuppressKeyPress = true;
+                e.Handled = true;
+            }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        public void button1_Click(object sender, EventArgs e)
         {
-
+            string json = File.ReadAllText(path);
+            var StatData = JsonConvert.DeserializeObject<Flashcard>(json);
+            List<Position> CatagorySearch = StatData.Position;
+            int[] Ranks = {0, 0, 0, 0, 0, 0};
+            foreach (Position Search in CatagorySearch)
+            {
+                Ranks[Search.Catagory]++;
+            }
+            MessageBox.Show($"Total: {CatagorySearch.Count} \nRank 1: {Ranks[5]} ({(int)Math.Round((decimal)100 * Ranks[5] / CatagorySearch.Count)}%) \nRank 2: {Ranks[4]} ({(int)Math.Round((decimal)100 * Ranks[4] / CatagorySearch.Count)}%) \nRank 3: {Ranks[3]} ({(int)Math.Round((decimal)100 * Ranks[3] / CatagorySearch.Count)}%) \nRank 4: {Ranks[2]} ({(int)Math.Round((decimal)100 * Ranks[2] / CatagorySearch.Count)}%) \nRank 5: {Ranks[1]} ({(int)Math.Round((decimal)100 * Ranks[1] / CatagorySearch.Count)}%) \nUnranked: {Ranks[0]} ({(int)Math.Round((decimal)100 * Ranks[0] / CatagorySearch.Count)}%) \n\n*NOTE: Each rank is 20%", "Stats");
         }
 
         private void button3_Click(object sender, EventArgs e)
